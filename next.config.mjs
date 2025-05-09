fc3c4193cce81963c75907dee6dc455307af9937
tyron40 <robertstyron40@gmail.com>
2025-05-09 17:19:40 -0400

    mobile view screen

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,19 @@
 const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
-    unoptimized: true,
+    unoptimized: false,
+    deviceSizes: [256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ["@nextui-org/react"],
   },
 }
 
